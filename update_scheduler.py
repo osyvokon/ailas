b24c634ee2b2336a925c27e3d7e9d9692a7d7572
Mike <darkcore27@gmail.com>
2015-10-04 01:54:46 +0300

    switched to 3.4

--- update_scheduler.py
+++ update_scheduler.py
@@ -8,26 +8,28 @@ db = MongoClient().ailas
 MASTER_SERVER_URL = ''
 
 
-async def update_session(loop, url, session_id, client_url):
+@asyncio.coroutine
+def update_session(loop, url, session_id, client_url):
     ms_client = aiohttp.ClientSession(loop=loop)
     u_clietnt = aiohttp.ClientSession(loop=loop)
     async with ms_client.get(MASTER_SERVER_URL+str(session_id)) as get_response,
                u_client.post(client_url) as post_response:
-        data = yield await get_response.read()
-        yield await post_response.write(data)
+        data = yield from get_response.read()
+        yield from post_response.write(data)
 
 
-async def schedule_update(loop, update_period=15.0):
+@asyncio.coroutine
+def schedule_update(loop, update_period=15.0):
     end_time = loop.time() + update_period
     while True:
         if (loop.time() + 1.0) >= end_time:
             end_time += update_period
             for session in db.sessions.find():
                 for url in session['addrs']:
-                    yield await update_session(loop=loop,
+                    yield from update_session(loop=loop,
                                                session_id=session['id'],
                                                client_url=url)
-        yield await asyncio.sleep(1)
+        yield from asyncio.sleep(1)
 
 
 def main():
