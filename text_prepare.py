5e3e4f0f7dda4ef1f67d7054ae135590b21f9f4a
Mike <darkcore27@gmail.com>
2015-10-03 16:47:38 +0300

    word replacer

--- text_prepare.py
+++ text_prepare.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.5
 
 import re
+import build_dict
 
 
 def text_prepare(text):
@@ -9,7 +10,21 @@ def text_prepare(text):
     pattern_exp = re.compile(pattern, re.MULTILINE & re.LOCALE)
     return pattern_exp.findall(text)
 
-def modify_text(text):
-    def replace(word, rpl):
-        nonlocal text
-        re.sub(word, rpl, text)
+def modify_text(text, word, rpl='BANANA'):
+    lemmtizer = build_dict.Lemmatizer()
+
+    def replace(word):
+        nonlocal text, rpl
+        text = re.sub(word, rpl, text)
+
+    for current_word in text_prepare(text):
+        lemma = lemmtizer.lemma(current_word)
+        if lemma == word:
+            replace(current_word)
+
+    return text
+
+if __name__ == '__main__':
+    text = 'Яблуко було дуже смачне. Я люблю яблука.'
+    print(modify_text(text=text,
+                      word='яблуко'))
