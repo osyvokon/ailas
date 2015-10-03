4fe988ba8c3576fca0c48ced9f400f708e63a68e
brianbelljr <brian.bell.jr@gmail.com>
2015-10-03 18:35:11 +0300

    add identities for chat

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.get('/', function(req,res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    console.log(msg);
+    io.emit('chat message', msg);
   });
 });
 
