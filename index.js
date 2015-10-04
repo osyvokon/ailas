e2c76a646d3577d452082ff214297db114d0fbcc
brianbelljr <brian.bell.jr@gmail.com>
2015-10-04 13:22:38 +0300

    online users

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ var http = require('http').Server(app);
 var request = require('request');
 var io = require('socket.io')(http);
 var moment = require('moment');
+var people = {};
 
 //var API_URL = 'http://40.115.53.131:5000/api';
 var API_URL = 'http://localhost:5000/api';
+
+var computeNamelist = function() {
+	var namelist = [];
+	for (var socketIdKey in people) {
+	  if (people.hasOwnProperty(socketIdKey)) {
+	    namelist.push(people[socketIdKey].name);
+	  }
+	}
+	return namelist
+}
 
 
 app.get('/', function(req,res){
@@ -22,6 +33,8 @@ io.on('connection', function(socket){
     txt: 'Known commands: \guess hints'
   });
 
+  // NODE SERVER GETS A CHAT MESSAGE
+  // -------------------------------
   socket.on('chat message', function(msg){
     //var time = moment().format('MMMM Do YYYY, h:mm:ss a');
     var time = moment().format('h:mm:ss a');
@@ -43,6 +56,36 @@ io.on('connection', function(socket){
         });
       });
   });
+
+  // NODE SERVER GETS A JOIN MESSAGE
+  // -------------------------------
+  socket.on('join message', function(msg){
+ 	// Add user to dict
+  	people[socket.id] = { 
+  		'name': msg['person'] 
+  	}
+  	var namelist = computeNamelist();
+  	io.emit('join ack message', namelist);
+
+    // request.get(API_URL + '/session/test/scores',
+    //   function (error, response, body) {
+    //   	// Update scores
+    //     //////////
+    //   });
+  });
+
+  // NODE SERVER GETS A DISCONNECT MESSAGE
+  // -------------------------------------
+  socket.on("disconnect", function() {
+		if (typeof people[socket.id] !== "undefined") { //this handles the refresh of the name screen
+			console.log('disconnect: ' + people[socket.id].name);
+			// Remove from people dict
+			delete people[socket.id];
+			//send new message to update clients
+			var namelist = computeNamelist();
+  			io.emit('join ack message', namelist);
+		}
+	});
 });
 
 http.listen(3001, function(){
