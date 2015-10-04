6850f48ca2c33858e387bf6f78b61274e98c60e1
Mike <darkcore27@gmail.com>
2015-10-04 05:51:02 +0300

    py2 server for associations api

--- w2v_associations.py
+++ w2v_associations.py
@@ -2,14 +2,10 @@ from gensim.models import Word2Vec
 
 
 class W2V:
-    def __init__(self, model_file):
+    def __init__(self):
         self.model = Word2Vec.load('text_model')
 
     def get_similair(self, word):
         wu = word.decode('utf-8')
         q = self.model.most_similar(wu)
-        # print '*'*5
-        # for i in q:
-        #     print i[0], i[1]
-        # print '-'*20
         return q
